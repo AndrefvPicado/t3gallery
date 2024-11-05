d791c941e2cf044f171239d89230202d06466c45
André Picado <andre.picado@tecnico.ulisboa.pt>
2024-11-05 22:26:19 +0000

    force dynamic page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { db } from "~/server/db";
+export const dynamic = 'force-dynamic';
 
 const mockUrls = [
   "https://utfs.io/f/NJX2X6gQLcArrXwCGoqIcO2ZQ8PEJoH7TzqVAf3lRiDkXGU6",
