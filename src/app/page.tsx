b270a4d9bf4a34778728bf63eb25a81f326085c5
André Picado <andre.picado@tecnico.ulisboa.pt>
2024-11-05 22:06:02 +0000

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/NJX2X6gQLcArrXwCGoqIcO2ZQ8PEJoH7TzqVAf3lRiDkXGU6",
@@ -10,10 +11,12 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url
 }))
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
   return (
     <main>
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
         {[...mockImages].map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url}/>
