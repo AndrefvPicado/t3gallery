495d7fd894543d860f405ae202575c2eab620945
André Picado <andre.picado@tecnico.ulisboa.pt>
2024-11-05 23:48:16 +0000

    use auth for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,35 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 export const dynamic = 'force-dynamic';
 
-export default async function HomePage() {
+async function Images(){
   const images = await db.query.images.findMany({
     orderBy: (model, {desc}) => desc(model.id),
   });
   return (
-    <main>
-      <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap gap-4">
         {images.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url}/>
             <div>{image.name}</div>
           </div>
         ))}
-      </div>
+    </div>
+)}
+
+export default async function HomePage() {
+
+  return (
+    <main>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
