cc8874c6cc7aea9349ceb1e60d187169fa17f584
André Picado <andre.picado@tecnico.ulisboa.pt>
2024-11-06 22:23:41 +0000

    modal cleanup

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -3,6 +3,13 @@ import { getImage } from "~/server/queries";
 export default async function FullPageImageView(props: {id: number}){
   const image = await getImage(props.id);
     return (
-        <img src={image.url} alt={image.name} height={480} width={480}  style={{objectFit: "contain"}}/>
+      <div className="flex w-full h-full min-w-0">
+        <div className="flex-shrink flex justify-center items-center">
+        <img src={image.url} className="object-contain"/>
+        </div>
+        <div className="flex w-48 flex-col flex-shrink-0 border-l">
+          <div className="text-xl font-bold">{image.name}</div>
+        </div>
+      </div>
       )
   }
